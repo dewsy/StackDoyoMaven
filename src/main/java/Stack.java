c2fdc1bc4400d9d3252c4e7adb716f7ba7bb3e0d
dewsy <dewsyy@gmail.com>
2019-01-29 11:21:57 +0100

    Tests are in place

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -3,35 +3,48 @@ import java.lang.reflect.Array;
 public class Stack<T> {
 
     private T[] stack;
-    private int counter = 0;
+    private int counter = -1;
 
     @SuppressWarnings("unchecked")
-    public Stack(T type, int size) {
-        stack = (T[]) Array.newInstance(type.getClass().getComponentType(), size);
+    public Stack(Class<T> tClass, int size) {
+        stack = (T[]) Array.newInstance(tClass, size);
     }
 
 
     public void push(T item) throws StackOverflow {
-        if (stack.length == counter -1) {
+        if (stack.length == counter +1) {
             throw new StackOverflow();
         }
-        stack[counter] = item;
         counter++;
+        stack[counter] = item;
     }
 
 
     public T pop() throws StackUnderflow {
-        if (counter == 0) {
+        if (counter == -1){
             throw new StackUnderflow();
         }
         T itemInQuestion = stack[counter];
+        stack[counter] = null;
         counter--;
         return itemInQuestion;
     }
 
 
     public T peek() {
+        if (counter < 0) {
+            return null;
+        }
         return stack[counter];
     }
 
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (T i : stack){
+            builder.append(i);
+            builder.append(", ");
+        }
+        String stackString = builder.toString();
+        return stackString.substring(0, stackString.length() -2);
+    }
 }
--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -6,19 +6,54 @@ import static org.junit.jupiter.api.Assertions.*;
 class StackTest {
 
 
-    Stack testStack = new Stack(Integer.TYPE, 5);
+    Stack testStack = new Stack(Integer.class, 5);
 
 
     @Test
     void simplePushTest() {
         testStack.push(1);
+        testStack.push(1);
+        testStack.push(1);
+        assertEquals("1, 1, 1, null, null", testStack.toString());
     }
 
     @Test
-    void pop() {
+    void pushThrowsStackOverflow(){
+        testStack.push(1);
+        testStack.push(1);
+        testStack.push(1);
+        testStack.push(1);
+        testStack.push(1);
+        assertThrows(StackOverflow.class, ()-> {testStack.push(1);});
     }
 
     @Test
-    void peek() {
+    void popPopsTest() {
+        testStack.push(1);
+        testStack.push(1);
+        assertEquals(1, testStack.pop());
+    }
+
+    @Test
+    void popRemovesTest() {
+        testStack.push(1);
+        testStack.pop();
+        assertEquals("null, null, null, null, null", testStack.toString());
+    }
+
+    @Test
+    void popThrowsStackUnderflow(){
+        assertThrows(StackUnderflow.class, ()-> {testStack.pop();});
+    }
+
+    @Test
+    void emptyStackReturnsNullOnPeakTest() {
+        assertEquals(null, testStack.peek());
+    }
+
+    @Test
+    void peekTest(){
+        testStack.push(1);
+        assertEquals(1, testStack.peek());
     }
 }
